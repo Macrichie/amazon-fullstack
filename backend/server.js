e7c0b64ca4b2672e42e6f047f7179c9d27a2a424
Macrichie <makanjuolakunle@gmail.com>
2021-01-09 23:47:28 +0000

    created signin backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import bodyParser from "body-parser";
+
+dotenv.config()
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/amazonfs', {
@@ -11,24 +15,11 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/amazonfs'
 });
 
 const port = process.env.PORT || 5000;
-// const { products } = data;
 
-// get a product
-// app.get("/api/products/:id", (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   console.log(req.params.id);
-//   // console.log(product)
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product Not Found!" });
-//   }
-// });
-
-// get all products
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
+// parse application/json
+app.use(express.json())
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
